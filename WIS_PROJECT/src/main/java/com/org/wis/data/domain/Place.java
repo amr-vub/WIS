0b3c89d3819fc2e59d198b8a7de3608ef2f2add4
amr-vub <amragawad@gmail.com>
2014-12-15 21:04:02 +0100

    Fixing some relations

--- WIS_PROJECT/src/main/java/com/org/wis/data/domain/Place.java
+++ WIS_PROJECT/src/main/java/com/org/wis/data/domain/Place.java
@@ -17,29 +17,7 @@ public class Place {
 	@Id @GeneratedValue
 	int placeID;
 	// TODO : add attributes or remove
-
 	
-	@OneToMany(cascade = CascadeType.ALL,mappedBy = "place")
-	List<Event> events = new ArrayList<Event>();
-	
-	@ManyToOne @JoinColumn(nullable = true)
-	Location location;
-		
-	public List<Event> getEvents() {
-		return events;
-	}
-
-	public void setEvents(List<Event> events) {
-		this.events = events;
-	}
-
-	public Location getLocation() {
-		return location;
-	}
-
-	public void setLocation(Location location) {
-		this.location = location;
-	}
 
 	public int getPlaceID() {
 		return placeID;
